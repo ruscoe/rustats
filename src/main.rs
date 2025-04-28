c8a81dc2d6e86b98a0f641c4aa06ff208888c2c9
ruscoe <danruscoe@protonmail.com>
2025-04-28 15:39:13 -0700

    Add number of CPU cores.

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,23 @@ use std::io::{stdout};
 
 fn main() {
     let mut system = System::new_all();
+    extern crate num_cpus;
 
     loop {
-        // Refresh system info
+        // Refresh system info.
         system.refresh_all();
 
-        // Clear the terminal
+        // Clear the terminal.
         execute!(stdout(), Clear(ClearType::All)).unwrap();
 
-        // Print system information
+        // Print system information.
         println!("CPU Usage: {:.2}%", system.global_cpu_info().cpu_usage());
         println!("Total Memory: {} MB", system.total_memory() / 1024);
         println!("Used Memory:  {} MB", system.used_memory() / 1024);
         println!("Running Processes: {}", system.processes().len());
+        println!("Number of CPU cores: {}", num_cpus::get());
 
-        // Wait for 1 second
+        // Wait for 1 second.
         thread::sleep(Duration::from_secs(1));
     }
 }
